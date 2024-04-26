c186325fca171682e7db202bfa380df276e1db4b
Bryan Alexander Quintana <tryprex@gmail.com>
2024-04-26 12:00:22 -0500

    primer comit

---
+++ src/main/java/WAEC2Grupo11/WAEC2Grupo11/Waec2Grupo11Application.java
@@ -0,0 +1,13 @@
+package WAEC2Grupo11.WAEC2Grupo11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Waec2Grupo11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Waec2Grupo11Application.class, args);
+	}
+
+}
---
+++ src/test/java/WAEC2Grupo11/WAEC2Grupo11/Waec2Grupo11ApplicationTests.java
@@ -0,0 +1,13 @@
+package WAEC2Grupo11.WAEC2Grupo11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Waec2Grupo11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
